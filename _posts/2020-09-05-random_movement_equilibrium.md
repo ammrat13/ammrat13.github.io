c769b73cc115072ea579a12925e0a2051501a2a4
Ammar Ratnani <ammrat13@gmail.com>
2020-08-31 17:26:43 -0400

    Started Sept. 5

---
+++ _posts/2020-09-05-random_movement_equilibrium.md
@@ -0,0 +1,74 @@
+---
+title: Equilibrium in Normal  Random Movement
+libs: [mathjax]
+---
+
+<div class="mathjaxDeclarations">
+    @@\newcommand{\nl}{\\}@@
+</div>
+
+In my last post [last post]({{page.previous.url}}), I touched on my efforts to
+model how a simple "virus" moves through a population, mainly through the lens
+of differential equations. As part of that, I wanted to test my model of how the
+population moves over time. Intuitively, we'd expect the dynamics to follow the
+[Diffusion Equation](https://en.wikipedia.org/wiki/Diffusion_equation), which
+reduces to the [Heat Equation](https://en.wikipedia.org/wiki/Heat_equation) if
+the diffusivity is constant throught the domain.
+
+![Equilibrium of my simulated diffusion](/assets/2020/09/05/clip_nofilter.png)
+Imagine my surprise, then, when I simulated the population to a steady state and
+got the above distribution. It's inconsistent with the heat equation --- we'd be
+expecting a uniform equilibrium distribution from that. Instead, we get "clumps"
+on the edges of the simulation domain, as well as a "rarefaction" near them.
+
+Granted, the result makes sense. Look at the code that moves people around:
+{% highlight python %}
+dx = np.random.normal(scale=MOVE_SIGMA, size=(POP_SIZE,))
+population += dx
+population = np.clip(population, 0.0, 1.0)
+{% endhighlight %}
+A given person has a non-zero probability of winding up exactly on the boundary,
+unlike everywhere else in the domain. Really, the bounaries are accumulating
+everyone who overstepped them.
+
+The rarefactions are somewhat harder to explain. The best I can come up with is
+to compare it to the "unbounded" case. Imagine the simulation domain streteched
+to infinity in both directions. Then, with every person moving according to a
+normal distribution, having a constant population density would be an
+equilibrium state. When we bound the domain, however, we lose the contribution
+of everyone outside. This becomes especially noticible near the edges, where
+almost half of the influx would be coming from outside @@[0,1]@@.
+
+Of couse, I find these "clipping artifacts" undesireable, and I looked for ways
+to mitigate them. One idea I had was to reroll the positions of people who
+overstepped instead of simply clipping them to the edge.
+{% highlight python %}
+dx = np.random.normal(scale=MOVE_SIGMA, size=(POP_SIZE,))
+while True:
+    population += dx
+    outIdx = (population <= 0.0) | (population >= 1.0)
+    numOut = np.sum(outIdx)
+    if numOut != 0:
+        population[outIdx] -= dx[outIdx]
+        dx[~outIdx] = 0.0
+        dx[outIdx] = np.random.normal(scale=MOVE_SIGMA, size=(numOut,))
+    else:
+        break
+{% endhighlight %}
+This did get rid of the people clumped to the edge, but it still left the
+rarefactions. In fact, it actually made them much worse.
+![Equilibrium with rerolling](/assets/2020/09/05/reroll.png)
+
+On a side note, decreasing the variance for people's movement seemed to help
+significantly. In my simulation, all the people moved according to a normal
+distribution, and the above figures were collected with @@\texttt{MOVE_SIGMA} =
+\sigma = 0.1@@. This value can be made arbitrarily small --- if, every second,
+people move according to a normal distribution with variance @@\sigma^2@@,
+simply do @@n@@ "runs" with variance @@\frac{1}{n}\sigma^2@@ to simulate one
+second, where @@n@@ can be made arbitrarily large.
+
+That works in theory, but not in practice. If we have a population of size
+@@\texttt{POP_SIZE} = K@@ and we simulate for @@t@@ seconds, then simulating
+takes @@\mathcal{O}(ntK)@@ time. That's not great if we plan to make @@n@@
+ridiculously large. However, we only really want to see the equilibrium state
+for now, and it might be better to calculate it rather than to simulate it out.
---
+++ assets/2020/09/05/simulation/random_movement.py
@@ -0,0 +1,161 @@
+import enum
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Utility classes for typing the parameters
+class BoundModeT(enum.Enum):
+    CLIP = enum.auto()
+    REROLL = enum.auto()
+
+# Whether or not to even simulate the population
+SIM_POP: bool = True
+# How many people to simulate
+POP_SIZE: int = 10000000
+# How many times to move them to get to a steady state
+NUM_STEPS: int = 30
+# What standard deviation the people should move with
+MOVE_SIGMA: float = 0.1
+# How to handle people moving out of bounds
+BOUND_MODE: BoundModeT = BoundModeT.REROLL
+# How many bins to show when displaying
+NUM_BINS: int = 100
+
+
+# Derived parameters
+BIN_DELTA: float = 1.0 / NUM_BINS
+
+# Utility functions
+def erfInt(x: float):
+    # Antiderivative of math.erf
+    return x * math.erf(x) + math.exp(-x**2) / math.sqrt(math.pi)
+
+
+# Compute the expected values as eigenvectors of a markov matrix
+# Note the indexing convention - rows are destinations and columns are sources
+# This is done for easier eigenvector computation later
+# Also note that we reuse computations for different modes
+
+markovMat: np.ndarray = np.empty( (NUM_BINS+2, NUM_BINS+2) )
+
+if BOUND_MODE == BoundModeT.CLIP or BOUND_MODE == BoundModeT.REROLL:
+
+    # Probability of going into the boundaries and clipping
+    markovMat[0][0] = markovMat[-1][-1] = 0.5
+    markovMat[-1][0] = markovMat[0][-1] = 1/2 * (1 + math.erf(-1 / (math.sqrt(2) * MOVE_SIGMA)))
+    for b in range(1, NUM_BINS+1):
+        # Compute the coefficients needed to calculate the integral
+        intScale = MOVE_SIGMA / (math.sqrt(2.0) * BIN_DELTA)
+        bScale = -BIN_DELTA / (math.sqrt(2) * MOVE_SIGMA)
+        # Use symmetry
+        markovMat[0][b] = 1/2 - intScale * (erfInt(b*bScale) - erfInt((b-1)*bScale))
+        markovMat[-1][NUM_BINS+1-b] = markovMat[0][b]
+
+    # Probability of escaping the boundaries with clipping
+    for b in range(1, NUM_BINS+1):
+        # Compute coefficients
+        bScale = BIN_DELTA / (math.sqrt(2.0) * MOVE_SIGMA)
+        # Again, leverage symmetry
+        markovMat[b][0] = 1/2 * (math.erf(b*bScale) - math.erf((b-1)*bScale))
+        markovMat[NUM_BINS+1-b][-1] = markovMat[b][0]
+
+    # Populate everything else
+    for B in range(1, NUM_BINS+1):
+        for b in range(1, NUM_BINS+1):
+            # The coefficients out front
+            intScale = MOVE_SIGMA / (math.sqrt(2.0) * BIN_DELTA)
+            bBScale = BIN_DELTA / (math.sqrt(2.0) * MOVE_SIGMA)
+            # The four terms in the integral
+            t1P = erfInt( bBScale * (B - (b-1)) )
+            t1N = erfInt( bBScale * ((B-1) - (b-1)) )
+            t2P = -erfInt( bBScale * (B - b) )
+            t2N = -erfInt( bBScale * ((B-1) - b))
+            # The final value
+            markovMat[b][B] = intScale * ( (t1P-t1N) + (t2P-t2N) )
+
+    # If we aren't going to clip, removed those entries form the matrix
+    # Renormalize everything else to have a probability of one
+    if BOUND_MODE == BoundModeT.REROLL:
+        # Remove clip states
+        markovMat = np.delete(markovMat, [0,-1], axis=0)
+        markovMat = np.delete(markovMat, [0,-1], axis=1)
+        # Renormalize
+        markovMat /= np.sum(markovMat, axis=0)
+
+
+# Solve for the eigenvalue 1
+# We already know it exists, so don't try to compute it
+markovMatSize = markovMat.shape[0]
+solMat = (markovMat - np.identity(markovMatSize))
+solVec = np.zeros((markovMatSize,))
+# When solving for the eigenvector, choose the one with a given population size
+# - sum of entries is given
+solMat[-1] = np.ones((markovMatSize,))
+solVec[-1] = POP_SIZE
+# Solve
+sol = np.linalg.solve(solMat, solVec)
+
+# Generate the expected distribution
+expectedX: np.ndarray = np.linspace(0.0, 1.0, NUM_BINS)
+expectedY: np.ndarray = None
+expectedNumClipped: float = None
+if BOUND_MODE == BoundModeT.CLIP:
+    expectedY = sol[1:-1]
+    expectedNumClipped = sol[0] + sol[-1]
+elif BOUND_MODE == BoundModeT.REROLL:
+    expectedY = sol
+
+
+# Only simulate if we were instructed to
+if SIM_POP:
+    # Generate our population on the unit interval
+    population: np.ndarray = np.random.uniform( size=(POP_SIZE,) )
+    # Move them around to get to a steady state
+    for i in range(NUM_STEPS):
+        # Move around, minding how we bound
+        # Emulate a do-while loop for this
+        dx = np.random.normal(scale=MOVE_SIGMA, size=(POP_SIZE,))
+        while True:
+            population += dx
+            # If we're clipping, clip and be done with it
+            if BOUND_MODE == BoundModeT.CLIP:
+                population = np.clip(population, 0.0, 1.0)
+                break
+            # If we should reroll, reroll those we need
+            elif BOUND_MODE == BoundModeT.REROLL:
+                outIdx = (population <= 0.0) | (population >= 1.0)
+                numOut = np.sum(outIdx)
+                if numOut != 0:
+                    population[outIdx] -= dx[outIdx]
+                    dx[~outIdx] = 0.0
+                    dx[outIdx] = np.random.normal(scale=MOVE_SIGMA, size=(numOut,))
+                else:
+                    break
+
+
+
+# Print the proportion clipped if we're clipping
+if BOUND_MODE == BoundModeT.CLIP:
+    # Find who got clipped and how many did
+    clippedIdx = (population == 0.0) | (population == 1.0)
+    numClipped = np.sum(clippedIdx)
+    # Print the results
+    print(f"Clipped {numClipped} vs {expectedNumClipped} " + \
+        f"({100.0 * abs(numClipped - expectedNumClipped) / (expectedNumClipped)}% Error)")
+    # Remove the clipped people from the histogram
+    population = np.delete(population, clippedIdx)
+
+# Create the plot
+fig, ax = plt.subplots()
+# Plot the histogram (if we need to) and the expected distribution
+if SIM_POP:
+    ax.hist(population, bins=NUM_BINS)
+#ax.plot(expectedX, expectedY)
+# Format the results
+ax.set_frame_on(True)
+ax.set_xlim(0.0, 1.0)
+ax.set_ylim(0.0, None)
+# Show
+plt.show()
